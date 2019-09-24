cb79f8e18695ff9d68d5e582017f87c3a0117561
frantz2027 <54542735+frantz2027@users.noreply.github.com>
2019-09-24 10:31:15 -0500

    Added Rock paper scissors

--- src/Assignments/RockPaperScissors.java
+++ src/Assignments/RockPaperScissors.java
@@ -1,7 +1,58 @@
 package Assignments;
 
+import javax.swing.*;
+import java.util.Random;
+
 public class RockPaperScissors {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int random = rand.nextInt(3) + 1;
+        String choice;
+        int choiceNumber;
+
+        choice = JOptionPane.showInputDialog(null, "Enter 1 for paper, 2 for rock, and 3 for scissors.");
+        choiceNumber = Integer.parseInt(choice);
+
+        if(choiceNumber == 1 && random == 1)
+        {
+            JOptionPane.showMessageDialog(null, "Paper against paper = tie!");
+        }
+        if(choiceNumber == 1 && random == 2)
+        {
+            JOptionPane.showMessageDialog(null, "Paper against rock = you win!");
+        }
+        if(choiceNumber == 1 && random == 3)
+        {
+            JOptionPane.showMessageDialog(null, "Paper against scissors = you lose!");
+        }
+        if(choiceNumber == 2 && random == 1)
+        {
+            JOptionPane.showMessageDialog(null, "Rock against paper = you lose!");
+        }
+        if(choiceNumber == 2 && random == 2)
+        {
+            JOptionPane.showMessageDialog(null, "Rock against rock = tie!");
+        }
+        if(choiceNumber == 2 && random == 3)
+        {
+            JOptionPane.showMessageDialog(null, "Rock against scissors = you win!");
+        }
+        if(choiceNumber == 3 && random == 1)
+        {
+            JOptionPane.showMessageDialog(null, "Scissors against paper = you win!");
+        }
+        if(choiceNumber == 3 && random == 2)
+        {
+            JOptionPane.showMessageDialog(null, "Scissors against rock = you lose!");
+        }
+        if(choiceNumber == 3 && random == 3)
+        {
+            JOptionPane.showMessageDialog(null, "Scissors against scissors = tie!");
+        }
+
 
     }
+
 }
+
+
